93138b05cd718afb168b18518808ee089b73c1ee
AlexeyZelenko <zelenkooleksii75@gmail.com>
2021-04-04 16:43:12 +0300

    04/04/2021

--- src/views/InfoProduct.vue
+++ src/views/InfoProduct.vue
@@ -1,9 +1,12 @@
 <template>
   <div
       style="margin: 30px"
-      class="list-group"
+      class="list-group py-4"
+      v-for="info in itemInfoProduct"
+      :key="info.id"
   >
     <router-link
+        class="py-2"
         :to="{name: 'Home'}"
     >
       <button
@@ -13,32 +16,45 @@
         {{ $t('ButtonBack.value', { value: '' }) }}
       </button>
     </router-link>
-      <div class="d-flex w-100 justify-content-between">
-        <h5 class="mb-1">{{$route.params.title}}</h5>
-        <small>Vue 3</small>
+
+<!--    Название сайта-->
+    <a class="list-group-item list-group-item-action active m-md-2" aria-current="true">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">{{$route.params.title}}</h5>
+    </div>
+    </a>
+
+<!--    Описание сайта-->
+    <div
+        v-for="item in info.arrayInfo"
+        :key="item"
+    >
+      <div
+          class="d-flex w-100 justify-content-between m-md-2"
+      >
+        <h5 class="mb-1">{{item.name}}</h5>
+        <small>{{item.id}}</small>
       </div>
       <p
           class="mb-1"
       >
-        Выбрали Vue3, так ка очень хотелось обуздать новую технологию)
+        {{item.description}}
       </p>
-      <small class="text-muted">В некоторых местах использоавли гремучую смесь Vue2 + Vue3</small>
+      <a href="https://getbootstrap.com/">
+      <button
+          type="button"
+          class="btn btn-outline-primary m-2"
 
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">List group item heading</h5>
-      <small class="text-muted">3 days ago</small>
+      >
+        >>>
+      </button>
+      </a>
+      <small
+          class="text-muted m-md-2"
+      >
+        {{item.description2}}
+      </small>
     </div>
-    <p class="mb-1">Some placeholder content in a paragraph.</p>
-    <small class="text-muted">And some muted small print.</small>
-
-
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">List group item heading</h5>
-      <small class="text-muted">3 days ago</small>
-    </div>
-    <p class="mb-1">Some placeholder content in a paragraph.</p>
-    <small class="text-muted">And some muted small print.</small>
-
 
   </div>
 </template>
@@ -51,6 +67,182 @@ export default {
       type: String,
     }
   },
+  data: () => ({
+    sections: [
+      {
+        id: 1,
+        title: 'bv.ck.ua',
+        link: 'https://bv.ck.ua',
+        text: 'Vue3, TypeScript, Firebase, Vuex, GoogleDisk, API Youtube, API Google Maps, собственные NPM',
+        sectionClass: 'sec1',
+        seen: true,
+        arrayInfo: [
+          {
+            id: 1,
+            name: 'VUE3',
+            description: 'Выбрали Vue3, так ка очень хотелось обуздать новую технологию)',
+            description2: 'https://v3.vuejs.org/',
+          },
+          {
+            id: 2,
+            name: 'FIREBASE',
+            description: 'Firebase - прекрасная технология от Google. Очень много крутых фишек. Например база данных в реальгом времени.',
+            description2: 'https://firebase.google.com/'
+          },
+          {
+            id: 3,
+            name: 'BOOTSTRAP',
+            description: 'Быстро создавайте и настраивайте адаптивные сайты, ориентированные на мобильные устройства, с помощью Bootstrap, самого популярного в мире инструментария с открытым исходным кодом, включающего переменные и миксины Sass, гибкую сеточную систему, обширные готовые компоненты и мощные плагины JavaScript.\n' +
+                '\n',
+            description2: 'https://getbootstrap.com/'
+          }
+        ],
+      },
+      {
+        id: 2,
+        title: 'nask.com.ua',
+        link: 'https://nask.com.ua',
+        text:'Vue2, Nuxt, Vuex, Video, Music',
+        sectionClass: 'sec2',
+        seen: true,
+        arrayInfo: [
+          {
+            id: 1,
+            name: 'VUE2',
+            description: 'Vue - шустрый, производительный, разносторонний.',
+            description2: 'https://v3.vuejs.org/',
+          },
+          {
+            id: 2,
+            name: 'NUXT',
+            description: 'Очень помогает SEO. Роботы, ищущие информацию в интернете, от него в восхищении.',
+            description2: 'https://ru.nuxtjs.org/',
+          },
+          {
+            id: 3,
+            name: 'FIREBASE',
+            description: 'Firebase - прекрасная технология от Google. Очень много крутых фишек. Например база данных в реальгом времени.',
+            description2: 'https://firebase.google.com/'
+          },
+          {
+            id: 4,
+            name: 'VUETIFY',
+            description: 'Vuetify - это библиотека пользовательского интерфейса Vue с красиво созданными вручную компонентами материалов.',
+            description2: 'https://vuetifyjs.com'
+          }
+        ],
+      },
+      {
+        id: 3,
+        title: 'teploservis68.com.ua',
+        link: 'https://teploservis68.com.ua',
+        text: 'Nuxt, Vue2, Firebase, Google Аналитика, Vuetify',
+        sectionClass: 'sec3',
+        seen: false,
+        arrayInfo: [
+          {
+            id: 1,
+            name: 'VUE2',
+            description: 'Vue - шустрый, производительный, разносторонний.',
+            description2: 'https://v3.vuejs.org/',
+          },
+          {
+            id: 2,
+            name: 'NUXT',
+            description: 'Очень помогает SEO. Роботы, ищущие информацию в интернете, от него в восхищении.',
+            description2: 'https://ru.nuxtjs.org/',
+          },
+          {
+            id: 3,
+            name: 'FIREBASE',
+            description: 'Firebase - прекрасная технология от Google. Очень много крутых фишек. Например база данных в реальгом времени.',
+            description2: 'https://firebase.google.com/'
+          },
+          {
+            id: 4,
+            name: 'VUETIFY',
+            description: 'Vuetify - это библиотека пользовательского интерфейса Vue с красиво созданными вручную компонентами материалов.',
+            description2: 'https://vuetifyjs.com'
+          }
+        ],
+      },
+      {
+        id: 4,
+        title: 'puh-g.ck.ua',
+        link: 'https://puh-g.ck.ua/',
+        text: 'Vue3, Vite, Bootstrap',
+        sectionClass: 'sec4',
+        seen: false,
+        arrayInfo: [
+          {
+            id: 1,
+            name: 'VUE3',
+            description: 'Выбрали Vue3, так ка очень хотелось обуздать новую технологию)',
+            description2: 'https://v3.vuejs.org/',
+          },
+          {
+            id: 2,
+            name: 'FIREBASE',
+            description: 'Firebase - прекрасная технология от Google. Очень много крутых фишек. Например база данных в реальгом времени.',
+            description2: 'https://firebase.google.com/'
+          },
+          {
+            id: 3,
+            name: 'BOOTSTRAP',
+            description: 'Быстро создавайте и настраивайте адаптивные сайты, ориентированные на мобильные устройства, с помощью Bootstrap, самого популярного в мире инструментария с открытым исходным кодом, включающего переменные и миксины Sass, гибкую сеточную систему, обширные готовые компоненты и мощные плагины JavaScript.\n' +
+                '\n',
+            description2: 'https://getbootstrap.com/'
+          },
+          {
+            id: 4,
+            name: 'VITE',
+            description: 'Инструменты нового поколения для внешнего интерфейса',
+            description2: 'https://vitejs.dev/'
+          }
+        ],
+      },
+      {
+        id: 5,
+        title: 'shop',
+        link: 'https://online-shop-34af2.web.app/',
+        text: 'Vue2, Firebase, Google Аналитика, Vuetify',
+        sectionClass: 'sec5',
+        seen: false,
+        arrayInfo: [
+          {
+            id: 1,
+            name: 'VUE2',
+            description: 'Vue - шустрый, производительный, разносторонний.',
+            description2: 'https://v3.vuejs.org/',
+          },
+          {
+            id: 2,
+            name: 'Google Аналитика',
+            description: 'Google Analytics - ваша кровь. Когда возникает аномалия или она выходит из строя, вы хотите знать об этом немедленно.',
+            description2: 'https://analytics.google.com/',
+          },
+          {
+            id: 3,
+            name: 'FIREBASE',
+            description: 'Firebase - прекрасная технология от Google. Очень много крутых фишек. Например база данных в реальгом времени.',
+            description2: 'https://firebase.google.com/'
+          },
+          {
+            id: 4,
+            name: 'VUETIFY',
+            description: 'Vuetify - это библиотека пользовательского интерфейса Vue с красиво созданными вручную компонентами материалов.',
+            description2: 'https://vuetifyjs.com'
+          }
+        ],
+      },
+    ]
+  }),
+  computed: {
+    itemInfoProduct() {
+      return this.sections.filter(item => item.title === this.title)
+
+      }
+  }
 }
 </script>
 
