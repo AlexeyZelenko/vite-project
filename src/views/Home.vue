74616c530f42935aa43bca2c70daa5b3a0b96d1a
Alexey Zelenko <zelenlooleksii.75@gmail.com>
2024-08-07 15:07:52 +0300

    Deleted item

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -57,6 +57,14 @@ export default {
         sectionClass: 'sec5',
         seen: false
       },
+      {
+        id: 6,
+        title: 'test',
+        link: 'https://online-shop-34af2.web.app/',
+        text: 'Vue2, Firebase, Google Аналитика, Vuetify',
+        sectionClass: 'sec5',
+        seen: false
+      },
     ]
   }),
 }
