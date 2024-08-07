d6efa3313b54487abb9026792e0195ccc1f4ade2
Alexey Zelenko <zelenlooleksii.75@gmail.com>
2024-08-07 15:05:01 +0300

    test2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -57,14 +57,6 @@ export default {
         sectionClass: 'sec5',
         seen: false
       },
-      {
-        id: 6,
-        title: 'test',
-        link: 'https://online-shop-34af2.web.app/',
-        text: '',
-        sectionClass: 'sec6',
-        seen: false
-      },
     ]
   }),
 }
