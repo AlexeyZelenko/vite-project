040a93c831fbcf00990a481f78a6a8f52c200e0c
Oleksii <zelenkooleksii75@gmail.com>
2024-10-28 16:40:43 +0200

    change person

---
+++ src/components/Person.vue
@@ -0,0 +1,15 @@
+<template>
+   <div>
+      <p>Person Component</p>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'Person',
+};
+</script>
+
+<style scoped>
+/* Можна додати стилі тут */
+</style>
